33a074ff4228be4fa43a7abce43df442c761b84a
vikas gupta <vikasaggrawal700@gmail.com>
2023-07-20 19:40:52 +0530

    Fetch data from api with more data

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { loadData } from "../utils/api";
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading("loading...");
+    setData(null);
+    setError(null);
+
+    loadData(url)
+      .then((res) => {
+        setLoading(false);
+        setData(res);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError({ message: "Something went wrong!", err });
+      });
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
