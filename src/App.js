49fd6afed5aa0a71817b6a0df58332050da9105a
vikas gupta <vikasaggrawal700@gmail.com>
2023-07-22 15:58:04 +0530

    Add api configurations of images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from "react";
+
+import { useStateMethonds } from "./hooks/useStateMethods";
+import useFetch from "./hooks/useFetch";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const { getApiConfiguration } = useStateMethonds();
 
-export default App
+  const { data } = useFetch("/configuration");
+  useEffect(() => {
+    if (data) {
+      const url = {
+        imageUrl: data.images.secure_base_url,
+        backDropImageSize: data.images.backdrop_sizes,
+        posterImageSize: data.images.poster_sizes,
+        profileImageSize: data.images.profile_sizes,
+      };
+      getApiConfiguration(url);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
+  return <div>App</div>;
+};
+
+export default App;
