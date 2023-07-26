2ad1edca5032883f6b1e3d69a283d0cd57e0cdf8
vikas gupta <vikasaggrawal700@gmail.com>
2023-07-26 19:40:24 +0530

    Add scroll button in cast section

--- src/components/subComponents/details/Cast.js
+++ src/components/subComponents/details/Cast.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useSelector } from "react-redux";
 
 import "../../../css/cast.css";
 import LoadImage from "../common/LoadImage";
 import avatar from "../../../images/avatar.png";
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
 
 const Cast = ({ data, loading }) => {
+  const castContainer = useRef();
   const {
     url: { imageUrl, profileImageSize },
   } = useSelector((state) => state.home);
+
+  const navigation = (dir) => {
+    const container = castContainer.current;
+
+    const scrollAmount =
+      dir === "left"
+        ? container.scrollLeft - (container.offsetWidth - 15 )
+        : container.scrollLeft + (container.offsetWidth - 15 );
+
+    container.scrollTo({
+      left: scrollAmount,
+      behavior: "smooth",
+    });
+  };
 
   const displayImage = (path) => {
     const location = path ? imageUrl + profileImageSize?.[3] + path : avatar;
@@ -16,7 +35,7 @@ const Cast = ({ data, loading }) => {
   };
 
   const showData = () => (
-    <div className="listItems">
+    <div className="listItems" ref={castContainer}>
       {data?.map((item, i) => (
         <div className="listItem" key={i}>
           <div className="profileImg">{displayImage(item?.profile_path)}</div>
@@ -38,6 +57,18 @@ const Cast = ({ data, loading }) => {
   };
   return (
     <div className="castSection container">
+      <BsFillArrowLeftCircleFill
+        className="carouselLeftNav arrow"
+        onClick={() => {
+          navigation("left");
+        }}
+      />
+      <BsFillArrowRightCircleFill
+        className="carouselRighttNav arrow"
+        onClick={() => {
+          navigation("right");
+        }}
+      />
       <div className="sectionHeading">Top Cast</div>
       {!loading ? (
         showData()
